b1ed0d5ebc4f5aebce7ef7f4133e909ce6e7ea59
Sudhanshu Jha <sjha@jcp.com>
2019-11-12 16:53:38 -0600

    first commit

---
+++ src/main/java/com/sudhanshu/example/reactspringdemo/ReactspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sudhanshu.example.reactspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactspringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactspringdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudhanshu/example/reactspringdemo/ReactspringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhanshu.example.reactspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactspringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
